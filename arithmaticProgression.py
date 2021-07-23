a2bb5d95564727c61bb01f09e52271d6e5995000
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-07-23 04:09:51 -0700

    Update arithmaticProgression.py

--- arithmaticProgression.py
+++ arithmaticProgression.py
@@ -1,12 +1,14 @@
 print('A.P')
-a = input('enter the first term')
+a = input('enter the first term: ')
 a = float(a)
 
-d = input('enter the common difference')
-d = float(b)
+d = input('enter the common difference: ')
+d = float(d)
 
-a + b = t2
+t2 = a + d
 
-t3=t2+b
+t3 = t2 + d
 
+t4 = t3 + d
 
+print(a,t2,t3,t4)
