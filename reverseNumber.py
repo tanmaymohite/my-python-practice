ecafc14d40b1eb5bfdb63718fbe4638aa01833be
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-07-23 18:32:20 +0530

    Update reverseNumber.py

--- reverseNumber.py
+++ reverseNumber.py
@@ -1,10 +1,16 @@
 #This is to print reverse of given number
 n=input('enter the number: ')
 n=int(n)
-s = 0
-while n > 0:
-    r = n % 10
-    s = s + r
-    n = n // 10
 
-print(n)
+while n > 0:
+     r = n % 10
+     print(r)
+     n = n // 10
+
+
+
+
+
+
+
+
