f30b6dad1777b6a41d8b6294dcf16d9bff5688e2
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-07-25 12:12:59 +0530

    centroid formula

---
+++ CentriodFormula.py
@@ -0,0 +1,30 @@
+#program of centroid formula
+print('\nprogram of centroid formula')
+
+print('\n(x1+x2+x3)/3,(y1+y2+y3)/3')
+print('\n')
+
+x1=input('Enter the value of x1: ')
+x1=int(x1)
+
+x2=input('Enter the value of x2: ')
+x2=int(x2)
+
+x3=input('Enter the value of x3: ')
+x3=int(x3)
+
+y1=input('Enter the value of y1: ')
+y1=int(y1)
+
+y2=input('Enter the value of y2: ')
+y2=int(y2)
+
+y3=input('Enter the value of y3: ')
+y3=int(y3)
+
+x=(x1+x2+x3)/3
+y=(y1+y2+y3)/3
+
+print('\nAns = '+str(x),str(y))
+
+
