74056440f49d4af5bbf62229f1810a352e92cc15
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-08-27 23:25:31 +0530

    dict

--- dic.py
+++ dic.py
@@ -1,11 +1,34 @@
-thisdict = {
-    'brand':'TATA',
-    'model':'Evision',
-    'year':'2021',
-    
+cars  ={
+    'car1':{
+    'brand':'tesla',
+    'model':'s',
+    'year':'2020'
+    },
+    'car2':{
+  'brand':'tata',
+   'model':'Evision',
+   'year':'2022'
+},
+    'car3':{
+        'brand':'suzuki',
+        'model':'alto K10',
+        'year':'2014'
+    },
+    'car4':{
+        'brand':'Kia',
+        'model':'selton',
+        'year':2019
+
+    }
 }
 
-x = thisdict.keys()
-print(x)
-thisdict['Colour'] = 'white'
-print(x)
+cars.update({'colour':'white'})
+cars.update({'colour':'black'})
+for x ,y in cars.items():
+    print(x,y)
+
+
+
+
+
+
--- practice.py
+++ practice.py
@@ -1,5 +1,55 @@
-t = 1
-while t <= 100:
+print('                  ----------Converter----------')
+
+print('''\nConvert minute into second,  kilometre into metre, feet into inches,etc   ''')
+print('\nFOR-')
+operators = ['\nMinute into Second = 1 ','second into minute = 2','kilometre into metre = 3','Metre into kilometre = 4','feet into inches = 5','inches into feet = 6']
+
+
+for t in operators :
     print(t)
-    t += 1
-print("done")
+
+print('\nPress particular number for particular function')
+a = input('\nEnter the function number: ')
+a = float(a)
+
+if a == 1:
+    b = input('enter the minute: ')
+    b = float(b)
+
+    b = b * 60
+    print('Here your minutes are converted into second: ' + str(b) + str('second'))
+if a == 2:
+    b = input('enter the seconds: ')
+    b = float(b)
+
+    b = b / 60
+    print('Here your seconds are converted into minute: ' + str(b)+str(' min'))
+
+elif a == 3:
+    c = input('Enter the kilometre: ')
+    c = float(c)
+
+    k = c*1000
+    print('Here your kilometre are converted into metre: ' + str(k)+str(' metre'))
+
+elif a == 4:
+    c = input('Enter the metre: ')
+    c = float(c)
+
+    k = c / 1000
+    print('Here your metre are converted into kilometre: ' + str(k) + str(' kilometre'))
+elif a == 5:
+    d = input('enter the feets: ')
+    d = float(d)
+
+    h = d*12
+    print('Here your feets are converted into inches: ' + str(h) + str('inches'))
+
+elif a == 6:
+    d = input('enter the inches: ')
+    d = float(d)
+
+    h = d / 12
+    print('Here your inches are converted into feets: ' + str(h) + str('feets'))
+
+
