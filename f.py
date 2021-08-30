abf2955fb420fcaa871bcd368fc0b28bf554749e
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-08-30 19:14:43 +0530

    Create f.py

---
+++ f.py
@@ -0,0 +1,16 @@
+def tanmay():
+    ta=['name:tanmay','age:16','country:india']
+    for t in ta:
+        print(t)
+def digvijay():
+    di = ['name:digvijay', 'age:20', 'country:india']
+    for f in di:
+        print(f)
+
+def line():
+    print('  #########################  ')
+
+
+tanmay()
+line()
+digvijay()
